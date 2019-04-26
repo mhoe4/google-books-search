f67ff5b7002c284164ae2b07fefe9302478e3a8b
Mark Hoefer <mhoefer1@gmail.com>
2019-04-26 14:51:01 -0400

    cleanup files && API.js

---
+++ client/src/utils/API.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API_Key = "AIzaSyCIb34O2N-1vt49FsukEIi6i3GZcD2WNpI";
+
+export default {
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  },
+  searchBooks: function(query) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_Key}`);
+  }   
+};
