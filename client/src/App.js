a4f57529067afd8f8da4e3009bbfaa7fea56cded
Mark Hoefer <mhoefer1@gmail.com>
2019-04-26 15:36:59 -0400

    nav component && pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/search";
+import Saved from "./pages/saved";
+import Nav from "./components/Nav";
 
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        WORK IN PROGRESS!
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
