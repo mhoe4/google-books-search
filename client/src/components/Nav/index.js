feabc43067b277c6dba070736ef3257c39aca9b2
Mark Hoefer <mhoefer1@gmail.com>
2019-05-05 19:22:59 -0400

    comment out console.log

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 class Nav extends Component {
-  componentDidMount() {
-    console.log(this.props.location);
-  }
+  // componentDidMount() {
+  //   console.log(this.props.location);
+  // }
 
   render() {
     return (
